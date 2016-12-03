fbab59c596dcf38bd1671174b63b1c6ea259a20f
BuildTools <unconfigured@null.spigotmc.org>
2016-12-03 15:07:47 -0500

    Added more back

--- com/gamsion/chris/vecsm/backend/Plan.java
+++ com/gamsion/chris/vecsm/backend/Plan.java
@@ -6,4 +6,29 @@ public class Plan {
 	public String title;
 	public String description;
 	public List<CostItems> items;
+	public Plan(){
+		
+	}
+	public Plan(String title){
+		this.title = title;
+	}
+	public Plan(String title, String description){
+		this.title = title;
+		this.description = description;
+	}
+	public Plan(String title, String description, CostItems... items){
+		this.title = title;
+		this.description = description;
+		for(CostItems i : items){
+			this.items.add(i);
+		}
+	}
+	public Plan(String title, String description, List<CostItems> items){
+		this.title = title;
+		this.title = title;
+		this.description = description;
+		this.items.addAll(items);
+	}
+
+	
 }
