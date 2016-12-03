07f23e69a882fda0ea7035142ba43d8f541237e0
BuildTools <unconfigured@null.spigotmc.org>
2016-12-03 14:44:34 -0500

    Added GUI components

---
+++ com/gamsion/chris/vecsm/gui/CreationGUI.java
@@ -0,0 +1,12 @@
+package com.gamsion.chris.vecsm.gui;
+
+import javax.swing.JButton;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+public class CreationGUI {
+	public JTextField title;
+	public JTextArea description;
+	public JButton create;
+	public JButton addItem;
+}
--- com/gamsion/chris/vecsm/gui/GUIFrame.java
+++ com/gamsion/chris/vecsm/gui/GUIFrame.java
@@ -4,33 +4,44 @@ import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Toolkit;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 
 public class GUIFrame extends JFrame{
 	Toolkit toolkit = Toolkit.getDefaultToolkit();
 	Dimension defaultsize = toolkit.getScreenSize();
 	private static final long serialVersionUID = 1L;
+	public JButton start = new JButton();
+	
 	
 	public GUIFrame(String s){
 		super(s);
-		setLayout(new FlowLayout());
-		setResizable(true);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setPreferredSize(new Dimension(defaultsize.width / 2 + 40,
-				defaultsize.height / 4));
-		pack();
-		setLocationRelativeTo(null);
-		setVisible(true);
+		this.setupGUI();
 	}
 	public GUIFrame(){
 		super("VECSM");
+		this.setupGUI();
+		
+	}
+	
+	protected void setupGUI(){
 		setLayout(new FlowLayout());
 		setResizable(true);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setPreferredSize(new Dimension(defaultsize.width / 2 + 40,
-				defaultsize.height / 4));
+				defaultsize.height / 2));
 		pack();
 		setLocationRelativeTo(null);
 		setVisible(true);
 	}
+	
+	
+	
+
+	
+	
+	
+	
+	
+	
 }
